c0a8fac0eedb267fe0c4c6f32e29a82ab6be5b32
Ibrahim Mammadov <zxyctn@gmail.com>
2023-11-10 11:26:10 +0100

    Run vite by default on 127.0.0.1

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,14 @@
+import dns from 'dns';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+dns.setDefaultResultOrder('verbatim');
+
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	server: {
+		host: '127.0.0.1',
+		port: 5173
+	}
 });
